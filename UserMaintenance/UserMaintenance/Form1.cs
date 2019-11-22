d9caf0f0c06941fd8619d45e28813395d5087a50
Tamás Fehér <tamas.feher@stud.uni-corvinus.hu>
2019-11-22 12:26:09 +0100

    tetszoleges uzenet

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -15,11 +15,11 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
